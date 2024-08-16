3a8d1867755e3cb487507fe8f615e7a55396ac75
Amit Perez <amitperez55@gmail.com>
2024-08-16 19:44:19 +0300

    continue working on Func

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const pages = document.querySelector('.pages-count');
 const days = document.querySelector('.days-count');
+const result = document.querySelector('result');
+const error = document.querySelector('errorMessage');
+
+function checkPagesOrDays(pages, days){
+    if ((pages && pages > 0) || (days && days > 0)){
+        console.log("Action Performed!");
+        result.innerHTML = (`{pages*days}`);
+    } else {
+        console.log("Please Try Again");
+        error.classList.remove();
+    }
+}
 
 const btn = document.querySelector('.checkResult');
 btn.addEventListener('click', (e) => {
     e.preventDefault();
-    if (pages.value === 0 || days.value === 0) {
-        console.log('please try again');
-    }
+    // checkPagesOrDays(pages.value, days.value);
 }) 
